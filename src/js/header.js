cde55ab4e4c59543dbbd305ad3c52093f3b2ccd1
Yash Aggarwal <yaggarwal@EX2INDIA.LOCAL>
2024-05-09 11:34:17 +0530

    archits code added

--- src/js/header.js
+++ src/js/header.js
@@ -157,14 +157,16 @@ SearchButton.addEventListener("click", function () {
   }
 
   data = data.join(" & ");
-
-  if (window.location.href === "https://armss.exitest.com/welcome.html") {
-    const encodedData = encodeURIComponent(JSON.stringify(data));
-    window.location.href = `data.html?data=${encodedData}&value=${value}`;
-  } else {
-    const encodedData = encodeURIComponent(JSON.stringify(data));
-    window.location.href = `data.html?data=${encodedData}&value=${value}`;
+  if (data.length > 0) {
+    if (window.location.href === "https://armss.exitest.com/welcome.html") {
+      const encodedData = encodeURIComponent(JSON.stringify(data));
+      window.location.href = `data.html?data=${encodedData}&value=${value}`;
+    } else {
+      const encodedData = encodeURIComponent(JSON.stringify(data));
+      window.location.href = `data.html?data=${encodedData}&value=${value}`;
+    }
   }
+
   // SearchItems.innerHTML = "";
 });
 
