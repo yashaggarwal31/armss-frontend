75befc2c1695ac466bb98807855c57203dcf963c
arguptaex <architgupta.rke@gmail.com>
2024-05-16 11:46:39 +0530

    chatspace scrollign to bottom added

--- src/scripts/chatbot.js
+++ src/scripts/chatbot.js
@@ -2,15 +2,15 @@ let ChatbotContainer = document.getElementById("ChatbotContainerMain");
 var btn = document.getElementById("Chatbot");
 let span = document.getElementById("closeChatbot");
 let ChatbotSuggestedQuestions = document.getElementById(
-  "ChatbotSuggestedQuestions"
+    "ChatbotSuggestedQuestions"
 );
 let querysearch = document.getElementById("querysearch");
 
 btn.onclick = function () {
-  ChatbotContainer.style.display = "flex";
+    ChatbotContainer.style.display = "flex";
 };
 span.onclick = function () {
-  ChatbotContainer.style.display = "none";
+    ChatbotContainer.style.display = "none";
 };
 
 const chatSpace = document.getElementById("Chat-space");
@@ -18,168 +18,173 @@ const chatSpace = document.getElementById("Chat-space");
 let userid = localStorage.getItem("Rsession_name");
 console.log(userid);
 send_data = {
-  query: "",
-  // "query": query,
-  resume_filters: {
-    Candidate: {
-      check: [],
+    query: "",
+    // "query": query,
+    resume_filters: {
+        Candidate: {
+            check: [],
+        },
+        Education: {
+            check: [],
+        },
+        WorkExperience: {
+            check: [],
+        },
+        Contact: {
+            check: [],
+        },
+        Skill: {
+            check: [],
+        },
+        ResumeIdList: {
+            check: [],
+            ResumeIdValue: [],
+        },
     },
-    Education: {
-      check: [],
-    },
-    WorkExperience: {
-      check: [],
-    },
-    Contact: {
-      check: [],
-    },
-    Skill: {
-      check: [],
-    },
-    ResumeIdList: {
-      check: [],
-      ResumeIdValue: [],
-    },
-  },
-  count: 0,
-  user: "userid1",
+    count: 0,
+    user: "userid1",
 };
 
 querysearch.addEventListener("keydown", (event) => {
-  if (event.key === "Enter" && event.target.value.length > 0) {
-    ChatbotSuggestedQuestions.style.display = "none";
-    value = event.target.value.replace(/\s+/g, " ").trim();
-    chat(value);
-    querysearch.value = "";
-  }
+    if (event.key === "Enter" && event.target.value.length > 0) {
+        ChatbotSuggestedQuestions.style.display = "none";
+        value = event.target.value.replace(/\s+/g, " ").trim();
+        chat(value);
+        querysearch.value = "";
+    }
 });
 
 function submitQuery(event) {
-  const queryInput = document.getElementById("queryInput");
-  if (!queryInput.value.trim()) {
-    event.preventDefault(); // Prevent form submission
-    alert("Please enter a query!"); // Show an alert or handle the empty input
-  } else {
-    var recentQuesDiv = document.getElementById("recent-ques");
-    recentQuesDiv.classList.add("hidden");
-    chat(event);
-  }
+    const queryInput = document.getElementById("queryInput");
+    if (!queryInput.value.trim()) {
+        event.preventDefault(); // Prevent form submission
+        alert("Please enter a query!"); // Show an alert or handle the empty input
+    } else {
+        var recentQuesDiv = document.getElementById("recent-ques");
+        recentQuesDiv.classList.add("hidden");
+        chat(event);
+    }
 }
 
-async function chat(value) {
-  const query = value;
-  const UserQuery = document.createElement("p");
-  UserQuery.classList.add("right");
-  UserQuery.textContent = query;
-  chatSpace.appendChild(UserQuery);
-  send_data["query"] = query;
-  console.log("archit1", send_data);
-  ////////////////////////////////////////
-  try {
-    // const response = await fetch("https://armss-be.exitest.com/chatbot", {
-    const response = await fetch("https://armss-be.exitest.com/chatbot", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(send_data),
-    });
-    const data = await response.json();
-    console.log("archit");
-    console.log("data fetched", data);
-    if (data[0] === "exit") {
-      ChatbotContainer.style.display = "none";
-    } else if (typeof data[0] === "string") {
-      console.log("check string");
-      const messageElement = document.createElement("p");
-      messageElement.classList.add("left");
-      messageElement.textContent = data[0];
-      chatSpace.appendChild(messageElement);
-    } else {
-      // resumes = data[0].slice(0, 10);
-      send_data["resume_filters"] = data[1];
-      send_data["count"] = data[2];
-      // let send_data_str = JSON.stringify(send_data);
-      let resumeids = JSON.stringify(data[0]);
 
-      FilteringData.chatbotResumeIds = resumeids;
-      FilteringData.chatbotData = true;
-      // sessionStorage.setItem("send_data", send_data_json);
-      console.log("after the mess");
-      console.log(send_data);
-      // let url = "resumeDisplay.html";
-      // // Redirect to the new URL
-      // // window.location.href = url;
-      // window.location.replace(url);
-      const messageElement = document.createElement("p");
-      messageElement.classList.add("left");
-      messageElement.textContent = "Your Results Displayed";
-      chatSpace.appendChild(messageElement);
-      FilteringData.page = "data";
-      ChatbotContainer.style.display = "none";
-      await triggerDOMContentLoaded();
+async function chat(value) {
+    const query = value;
+    const UserQuery = document.createElement("p");
+    UserQuery.classList.add("right");
+    UserQuery.textContent = query;
+    chatSpace.appendChild(UserQuery);
+    chatSpace.scrollTop = chatSpace.scrollHeight
+    send_data["query"] = query;
+    console.log("archit1", send_data);
+    ////////////////////////////////////////
+    try {
+        // const response = await fetch("https://armss-be.exitest.com/chatbot", {
+        const response = await fetch("https://armss-be.exitest.com/chatbot", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(send_data),
+        });
+        const data = await response.json();
+        console.log("archit");
+        console.log("data fetched", data);
+        if (data[0] === "exit") {
+            ChatbotContainer.style.display = "none";
+        } else if (typeof data[0] === "string") {
+            console.log("check string");
+            const messageElement = document.createElement("p");
+            messageElement.classList.add("left");
+            messageElement.textContent = data[0];
+            chatSpace.appendChild(messageElement);
+            chatSpace.scrollTop = chatSpace.scrollHeight
+        } else {
+            // resumes = data[0].slice(0, 10);
+            send_data["resume_filters"] = data[1];
+            send_data["count"] = data[2];
+            // let send_data_str = JSON.stringify(send_data);
+            let resumeids = JSON.stringify(data[0]);
+
+            FilteringData.chatbotResumeIds = resumeids;
+            FilteringData.chatbotData = true;
+            // sessionStorage.setItem("send_data", send_data_json);
+            console.log("after the mess");
+            console.log(send_data);
+            // let url = "resumeDisplay.html";
+            // // Redirect to the new URL
+            // // window.location.href = url;
+            // window.location.replace(url);
+            const messageElement = document.createElement("p");
+            messageElement.classList.add("left");
+            messageElement.textContent = "Your Results Displayed";
+            chatSpace.appendChild(messageElement);
+            chatSpace.scrollTop = chatSpace.scrollHeight
+
+            FilteringData.page = "data";
+            ChatbotContainer.style.display = "none";
+            await triggerDOMContentLoaded();
+        }
+        return data;
+    } catch (error) {
+        console.error("Error:", error);
     }
-    return data;
-  } catch (error) {
-    console.error("Error:", error);
-  }
-  ///////////////////////////////////////
-  // fetch("https://armss-be.exitest.com/chatbot", {
-  //     method: "POST",
-  //     headers: {
-  //         "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(send_data),
-  // })
-  //     .then((response) => response.json())
-  //     // .then((data) => {
-  //     //     console.log(data);
-  //     // })
-  //     .then((data) => {
-  //         console.log("archit")
-  //         console.log(data)
-  //         var queryInput = document.getElementById('queryInput');
-  //         queryInput.value = '';
-  //         // console.log(resumes.slice(0, 5))
-  //         // console.log(data[1].slice(0, 10));
-  //         // resumes.map((record) => {
-  //         //     const messageElement = document.createElement('p');
-  //         //     messageElement.classList.add('left');
-  //         //     messageElement.textContent = `${record[1]}  (${record[4].slice[0, 8]}) - ${record[5].slice[0, 8]}`;
-  //         //     chatSpace.appendChild(messageElement);
-  //         // });
-  //         console.log(data[0], typeof data[0])
-  //         if (data[0] === "exit") {
-  //             var modal = document.getElementById("myModal");
-  //             modal.style.display = "none";
-  //         }
-  //         else if (typeof data[0] === 'string') {
-  //             console.log("check string");
-  //             const messageElement = document.createElement('p');
-  //             messageElement.classList.add('left');
-  //             messageElement.textContent = data[0]
-  //             chatSpace.appendChild(messageElement);
-  //         } else {
-  //             //
-  //             resumes = data[0].slice(0, 10);
-  //             send_data["resume_filters"] = data[1]
-  //             send_data["count"] = data[2]
-  //             // updateJson(data[1], data[2])
-  //             let send_data_str = JSON.stringify(send_data);
-  //             localStorage.setItem('send_data', send_data_str);
-  //             sessionStorage.setItem('send_data', send_data_json);
-  //             console.log("after the mess")
-  //             console.log(send_data)
-  //             let url = 'resumeDisplay.html'
-  //             // Redirect to the new URL
-  //             // window.location.href = url;
-  //             // window.location.replace(url);
-  //         }
-  //         return data;
-  //     })
-  //     .catch((error) => {
-  //         console.error("Error:", error);
-  //     });
+    ///////////////////////////////////////
+    // fetch("https://armss-be.exitest.com/chatbot", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(send_data),
+    // })
+    //     .then((response) => response.json())
+    //     // .then((data) => {
+    //     //     console.log(data);
+    //     // })
+    //     .then((data) => {
+    //         console.log("archit")
+    //         console.log(data)
+    //         var queryInput = document.getElementById('queryInput');
+    //         queryInput.value = '';
+    //         // console.log(resumes.slice(0, 5))
+    //         // console.log(data[1].slice(0, 10));
+    //         // resumes.map((record) => {
+    //         //     const messageElement = document.createElement('p');
+    //         //     messageElement.classList.add('left');
+    //         //     messageElement.textContent = `${record[1]}  (${record[4].slice[0, 8]}) - ${record[5].slice[0, 8]}`;
+    //         //     chatSpace.appendChild(messageElement);
+    //         // });
+    //         console.log(data[0], typeof data[0])
+    //         if (data[0] === "exit") {
+    //             var modal = document.getElementById("myModal");
+    //             modal.style.display = "none";
+    //         }
+    //         else if (typeof data[0] === 'string') {
+    //             console.log("check string");
+    //             const messageElement = document.createElement('p');
+    //             messageElement.classList.add('left');
+    //             messageElement.textContent = data[0]
+    //             chatSpace.appendChild(messageElement);
+    //         } else {
+    //             //
+    //             resumes = data[0].slice(0, 10);
+    //             send_data["resume_filters"] = data[1]
+    //             send_data["count"] = data[2]
+    //             // updateJson(data[1], data[2])
+    //             let send_data_str = JSON.stringify(send_data);
+    //             localStorage.setItem('send_data', send_data_str);
+    //             sessionStorage.setItem('send_data', send_data_json);
+    //             console.log("after the mess")
+    //             console.log(send_data)
+    //             let url = 'resumeDisplay.html'
+    //             // Redirect to the new URL
+    //             // window.location.href = url;
+    //             // window.location.replace(url);
+    //         }
+    //         return data;
+    //     })
+    //     .catch((error) => {
+    //         console.error("Error:", error);
+    //     });
 }
 // data = {
 //     "query": "get me data on java",
@@ -221,19 +226,19 @@ async function chat(value) {
 //         console.error("Error:", error);
 //     });
 function addQuestion(button) {
-  console.log();
-  var paragraph = button.querySelector("p");
+    console.log();
+    var paragraph = button.querySelector("p");
 
-  var question = paragraph.textContent.replace(/\s+/g, " ").trim();
+    var question = paragraph.textContent.replace(/\s+/g, " ").trim();
 
-  console.log(question);
-  ChatbotSuggestedQuestions.style.display = "none";
-  chatSpace.style.minHeight = "40vh";
-  chat(question);
+    console.log(question);
+    ChatbotSuggestedQuestions.style.display = "none";
+    chatSpace.style.minHeight = "40vh";
+    chat(question);
 }
 function resetQuery() {
-  var queryInput = document.getElementById("queryInput");
-  queryInput.value = "";
+    var queryInput = document.getElementById("queryInput");
+    queryInput.value = "";
 }
 // window.send_data = send_data
 // chat()
