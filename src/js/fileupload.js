802ef2698679e70313c05779508eb02ea4dd469e
DarkNorth9999 <yashaggarwal0009@gmail.com>
2024-06-04 22:50:03 +0530

    notifications and upload

--- src/js/fileupload.js
+++ src/js/fileupload.js
@@ -1,77 +1,79 @@
 const UploadWrapperChild = document.getElementById(
   "upload-child-wrapper"
-).innerHTML;
+).innerHTML
 function fileuplodInit() {
-  document.getElementById('fileInput').disabled = false;
-  const uploadDiv = document.getElementById("upload-zone");
-  const fileInput = document.getElementById("fileInput");
-  const drivelink = document.getElementById("drivelink");
-  const filesList = document.getElementById("fileList");
-  const uploadResetBtn = document.getElementById("upload-reset");
-  const uploadMoreFiles = document.getElementById('upload-more-files');
+  document.getElementById("fileInput").disabled = false
+  const uploadDiv = document.getElementById("upload-zone")
+  const fileInput = document.getElementById("fileInput")
+  const drivelink = document.getElementById("drivelink")
+  const filesList = document.getElementById("fileList")
+  const uploadResetBtn = document.getElementById("upload-reset")
+  const uploadMoreFiles = document.getElementById("upload-more-files")
 
   uploadResetBtn.addEventListener("click", function (event) {
-    uploadReset();
-    document.getElementById("dialog").close();
-  });
+    currentUploadCancelled = true
+    uploadReset()
+    document.getElementById("continue-upload").style.display = "none"
+    document.getElementById("dialog").close()
+  })
 
-  uploadMoreFiles.addEventListener('click', function (event) {
-    uploadReset();
-    uploadMoreFiles.style.display = 'none';
+  uploadMoreFiles.addEventListener("click", function (event) {
+    uploadReset()
+    uploadMoreFiles.style.display = "none"
   })
 
   uploadDiv.addEventListener("click", function (event) {
     if (event.target !== drivelink) {
       // If not, trigger the file input click event
-      fileInput.click();
+      fileInput.click()
     }
-  });
+  })
 
   // Add drag-and-drop event listeners to the div
   uploadDiv.addEventListener("dragover", function (event) {
     // Prevent default behavior to allow dropping
-    event.preventDefault();
-    uploadDiv.classList.add("dragging");
-  });
+    event.preventDefault()
+    uploadDiv.classList.add("dragging")
+  })
 
   uploadDiv.addEventListener("dragleave", function () {
     // Remove dragging style when drag leaves the div
-    uploadDiv.classList.remove("dragging");
-  });
+    uploadDiv.classList.remove("dragging")
+  })
 
   uploadDiv.addEventListener("drop", function (event) {
     // Prevent default behavior and stop event propagation
-    event.preventDefault();
-    uploadDiv.classList.remove("dragging");
+    event.preventDefault()
+    uploadDiv.classList.remove("dragging")
 
     // Access the dropped files
-    const files = event.dataTransfer.files;
+    const files = event.dataTransfer.files
 
     // Set the dropped files in the file input element
-    fileInput.files = files;
+    fileInput.files = files
 
-    document.getElementById("FileRefCount").innerHTML = fileInput.files.length;
-    document.getElementById("FileRefCount-div").style.display = "block";
+    document.getElementById("FileRefCount").innerHTML = fileInput.files.length
+    document.getElementById("FileRefCount-div").style.display = "block"
 
     // Optional: Handle the file selection
     if (files.length > 0) {
-      console.log("Dropped files:", files);
+      console.log("Dropped files:", files)
       // Perform desired action with the files (e.g., uploading)
     }
-  });
+  })
 
   fileInput.addEventListener("change", function () {
-    document.getElementById("upload-btn").disabled = false;
-    document.getElementById("upload-btn").style.display = "block";
-    document.getElementById("upload-click-div").style.display = "none";
+    document.getElementById("upload-btn").disabled = false
+    document.getElementById("upload-btn").style.display = "block"
+    document.getElementById("upload-click-div").style.display = "none"
 
-    const files = fileInput.files;
+    const files = fileInput.files
     // console.log('files length', files.length)
-    document.getElementById("FileRefCount").innerHTML = files.length;
-    document.getElementById("FileRefCount-div").style.display = "block";
+    document.getElementById("FileRefCount").innerHTML = files.length
+    document.getElementById("FileRefCount-div").style.display = "block"
 
     // Clear previous list
-    filesList.innerHTML = "";
+    filesList.innerHTML = ""
 
     // if (files.length > 0) {
     //     // Apply CSS class when files are selected
@@ -89,107 +91,109 @@ function fileuplodInit() {
     //     // Remove CSS class if no files are selected
     //     uploadDiv.classList.remove('hasFiles');
     // }
-  });
+  })
 
   document
     .getElementById("upload-btn")
     .addEventListener("click", async function async(event) {
-      document.getElementById("upload-btn").disabled = true;
-      document.getElementById('fileInput').disabled = true;
-      document.getElementById('to-collapse-upload').style.display = 'none';
-      document.getElementById('upload-zone').classList.add('upload-zone-div-disabled')
-      document.getElementById('upload-more-files').style.display = 'block';
+      document.getElementById("upload-btn").disabled = true
+      document.getElementById("fileInput").disabled = true
+      document.getElementById("to-collapse-upload").style.display = "none"
+      document
+        .getElementById("upload-zone")
+        .classList.add("upload-zone-div-disabled")
+      document.getElementById("upload-more-files").style.display = "block"
 
-      event.preventDefault();
-      console.log("Upload button clicked, about to call upload function.");
-      await upload();
-    });
+      event.preventDefault()
+      console.log("Upload button clicked, about to call upload function.")
+      await upload()
+    })
 
-  let uploadedCount = 0;
-  let totalFileCount = 0;
-  let errorCount = 0;
-  let progressBarOverall;
-  let currentUploadCancelled = false;
-  const maxFileSize = 5 * 1024 * 1024; //2MB
-  const uploadingDIV = document.getElementById("upload-click-div");
-  const uploadCount = document.getElementById("uploadCount");
-  const totalCount = document.getElementById("totalCount");
-  const errorCounter = document.getElementById("errorCount");
-  const errorTotal = document.getElementById("errorTotal");
+  let uploadedCount = 0
+  let totalFileCount = 0
+  let errorCount = 0
+  let progressBarOverall
+  let currentUploadCancelled = false
+  const maxFileSize = 5 * 1024 * 1024 //2MB
+  const uploadingDIV = document.getElementById("upload-click-div")
+  const uploadCount = document.getElementById("uploadCount")
+  const totalCount = document.getElementById("totalCount")
+  const errorCounter = document.getElementById("errorCount")
+  const errorTotal = document.getElementById("errorTotal")
 
   document.getElementById("cancel-upload-btn").addEventListener("click", () => {
-    currentUploadCancelled = true;
-    document.getElementById("cancel-upload-btn").style.display = "none";
-    document.getElementById("cancelled").style.display = "inline";
-  });
+    currentUploadCancelled = true
+    document.getElementById("cancel-upload-btn").style.display = "none"
+    document.getElementById("cancelled").style.display = "inline"
+  })
 
   const uploadReset = () => {
-    document.getElementById("upload-parent-wrapper").innerHTML = "";
+    document.getElementById("upload-parent-wrapper").innerHTML = ""
     document.getElementById("upload-parent-wrapper").innerHTML =
-      UploadWrapperChild;
+      UploadWrapperChild
 
-    document.getElementById("upload-btn").disabled = false;
+    document.getElementById("upload-btn").disabled = false
 
-    fileuplodInit();
-  };
+    fileuplodInit()
+  }
 
   const upload = async () => {
-    currentUploadCancelled = false;
+    currentUploadCancelled = false
 
-    uploadedCount = 0;
-    totalFileCount = 0;
-    errorCount = 0;
+    uploadedCount = 0
+    totalFileCount = 0
+    errorCount = 0
 
-    const fileList = document.getElementById("fileInput").files;
-    if (fileList.length == 0) return;
-    const total = fileList.length;
-    totalFileCount = total;
-    console.log("ssssddfdvdvdvdvdvddvdv", totalFileCount);
+    const fileList = document.getElementById("fileInput").files
+    if (fileList.length == 0) return
+    const total = fileList.length
+    totalFileCount = total
+    console.log("ssssddfdvdvdvdvdvddvdv", totalFileCount)
 
-    const notificationId = await createNotification(fileList.length);
+    const notificationId = await createNotification(fileList.length)
 
-    uploadCount.innerHTML = uploadedCount;
-    totalCount.innerHTML = total;
+    uploadCount.innerHTML = uploadedCount
+    totalCount.innerHTML = total
 
-    uploadingDIV.style.display = "grid";
+    uploadingDIV.style.display = "grid"
 
-    document.getElementById("upload-btn").disabled = true;
-    document.getElementById("upload-btn").style.display = "none";
+    document.getElementById("upload-btn").disabled = true
+    document.getElementById("upload-btn").style.display = "none"
 
-    progressBarOverall = document.getElementById("progressBarOverall");
+    progressBarOverall = document.getElementById("progressBarOverall")
 
-    progressBarOverall.max = total;
+    progressBarOverall.max = total
 
     for (let i = 0; i < fileList.length; i++) {
       if (currentUploadCancelled) {
-        console.log("upload cancelled");
-        break;
+        console.log("upload cancelled")
+        break
       }
 
       //selecting individual file
-      const uploadedFile = fileList[i];
-      const fileHash = await calculateHash(uploadedFile);
+      const uploadedFile = fileList[i]
+      const fileHash = await calculateHash(uploadedFile)
       // Database query to check if file exists in db already *****pending
       //If so continue and dont proceed further
 
       // const file = fileList[i];
 
       const newName =
-        `[${notificationId}]-` + `${fileHash}` + `!@&` + uploadedFile.name;
+        `[${notificationId}]-` + `${fileHash}` + `!@&` + uploadedFile.name
       const file = new File([uploadedFile], newName, {
         type: uploadedFile.type,
-      });
+      })
 
-      console.log(fileHash);
+      console.log(fileHash)
 
       if (file.size > maxFileSize) {
-        alert("File Uploaded exceeds limit", file.name);
-        continue;
+        alert("File Uploaded exceeds limit", file.name)
+        continue
       }
 
-      const fileID = file.name + file.type + i;
+      const fileID = file.name + file.type + i
 
-      console.log(file.name);
+      console.log(file.name)
 
       try {
         // getUploadLink(file.name).then((link) => {
@@ -197,15 +201,15 @@ function fileuplodInit() {
         //   const response = uploadFileThroughLink(link.presignedUrl, file)
         // })
 
-        const link = await getUploadLink(file.name);
-        const response = await uploadFileThroughLink(link.presignedUrl, file);
-        console.log('response is ', response)
+        const link = await getUploadLink(file.name)
+        const response = await uploadFileThroughLink(link.presignedUrl, file)
+        console.log("response is ", response)
 
-        if (response == 'success') {
-          uploadedCount++;
+        if (response == "success") {
+          uploadedCount++
           if (progressBarOverall) {
-            progressBarOverall.value = uploadedCount;
-            uploadCount.innerHTML = uploadedCount;
+            progressBarOverall.value = uploadedCount
+            uploadCount.innerHTML = uploadedCount
           }
 
           console.log(
@@ -213,26 +217,23 @@ function fileuplodInit() {
             uploadedCount,
             "\nTotal File Count: ",
             totalFileCount
-          );
+          )
 
           if (uploadedCount === totalFileCount && errorCount === 0) {
-            document.getElementById("cancel-upload-div").style.display = "none";
+            document.getElementById("cancel-upload-div").style.display = "none"
             document.getElementById("successful-message").style.display =
-              "block";
-            console.log("Reached here");
+              "block"
+            console.log("Reached here")
           }
+        } else {
+          errorCount++
+          errorTotal.innerHTML = errorCount
+          document.getElementById("cancel-upload-div").style.display = "none"
+          errorCounter.style.display = "block"
         }
-        else {
-          errorCount++;
-          errorTotal.innerHTML = errorCount;
-          document.getElementById("cancel-upload-div").style.display = "none";
-          errorCounter.style.display = "block";
-        }
-
-
       } catch (error) {
-        errorCount++;
-        console.error(`Failed to upload file ${file.name}: ${error}`);
+        errorCount++
+        console.error(`Failed to upload file ${file.name}: ${error}`)
       }
 
       // if (i == fileList.length - 1) {
@@ -246,7 +247,7 @@ function fileuplodInit() {
     // setTimeout(() => {
     //
     // }, 20000)
-  };
+  }
 
   async function createNotification(fileCount) {
     try {
@@ -259,24 +260,24 @@ function fileuplodInit() {
           },
           body: JSON.stringify({ fileCount: fileCount }),
         }
-      );
+      )
 
-      const data = await response.json();
-      const notificationId = data["notification_id"];
+      const data = await response.json()
+      const notificationId = data["notification_id"]
 
-      console.log("Notification ID:", notificationId);
+      console.log("Notification ID:", notificationId)
 
-      return notificationId;
+      return notificationId
     } catch (error) {
-      console.error("Error fetching NotificationID:", error);
+      console.error("Error fetching NotificationID:", error)
     }
   }
 
   const getUploadLink = async (filename) => {
-    console.log(`this is filename, ${filename}`);
+    console.log(`this is filename, ${filename}`)
     const requestData = {
       name: filename,
-    };
+    }
 
     return fetch("https://armss-be.exitest.com/presignedUrl/", {
       method: "POST", // Change the method to POST
@@ -287,67 +288,74 @@ function fileuplodInit() {
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok");
+          throw new Error("Network response was not ok")
         }
-        return response.json();
+        return response.json()
       })
       .then((data) => {
         // Handle the fetched object here
-        console.log(data);
-        return data;
+        console.log(data)
+        return data
       })
       .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-        errorCount++;
-      });
-  };
+        console.error("There was a problem with the fetch operation:", error)
+        errorCount++
+      })
+  }
 
   const uploadFileThroughLink = async (url, file) => {
-
     try {
       const data = await fetch(url, {
         method: "PUT",
         body: file,
       })
 
-      console.log(data);
+      console.log(data)
 
       if (data.status === 200) {
-        return 'success'
+        return "success"
       } else {
-        return 'error'
+        return "error"
       }
+    } catch (error) {
+      console.error(error)
+      throw Error()
     }
-    catch (error) {
-      console.error(error);
-      throw Error();
-
-    }
-
-
-
-
-  };
+  }
 
   function calculateHash(file) {
     return new Promise((resolve, reject) => {
-      const reader = new FileReader();
+      const reader = new FileReader()
       reader.onload = (event) => {
-        const arrayBuffer = event.target.result;
-        const hash = sha256(new Uint8Array(arrayBuffer));
-        resolve(hash);
-      };
+        const arrayBuffer = event.target.result
+        const hash = sha256(new Uint8Array(arrayBuffer))
+        resolve(hash)
+      }
       reader.onerror = (error) => {
-        reject(error);
-      };
-      reader.readAsArrayBuffer(file);
-    });
+        reject(error)
+      }
+      reader.readAsArrayBuffer(file)
+    })
   }
 
   function sha256(arrayBuffer) {
-    const wordArray = CryptoJS.lib.WordArray.create(arrayBuffer);
-    return CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
+    const wordArray = CryptoJS.lib.WordArray.create(arrayBuffer)
+    return CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex)
   }
+
+  document.getElementById("continue-upload").addEventListener("click", () => {
+    console.log("continue clicked")
+    document.getElementById("dialog").showModal()
+    document.getElementById("continue-upload").style.display = "none"
+  })
+
+  document
+    .getElementById("upload-minimize-icon")
+    .addEventListener("click", () => {
+      console.log("minmize clicked")
+      document.getElementById("dialog").close()
+      document.getElementById("continue-upload").style.display = "flex"
+    })
 }
 
-fileuplodInit();
+fileuplodInit()
